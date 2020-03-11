2cf8a86aab9dd1dd92dfa73471fe315884f2e09b
pathas <pathas@naver.com>
2020-03-11 14:59:43 +0900

    Layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import Header from "./Layout/Header";
+import Navigation from "./Layout/Navigation";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <Layout>
+        <Header />
+        <Navigation />
+      </Layout>
+    </>
+  );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    margin:0px;
+    padding:0px;
+  }
+`;
+
+const Layout = styled.div`
+  margin: 0 auto;
+  display: flex;
+  width: 100%;
+  flex-flow: row wrap;
+`;
